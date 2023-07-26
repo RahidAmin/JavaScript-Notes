8edbfa4ab63cfc99872ba63a7894487d1a791e75
RahidAmin <rahidamin19@gmail.com>
2023-07-25 19:57:08 -0700

    dom

---
+++ Dom.js
@@ -0,0 +1,8 @@
+     
+     var changeElemant=document.getElementById("head3").innerText="Hello";
+
+    //  changeElemant.innerText="Hello hello";
+     console.log(changeElemant);
+
+     document.getElementsByTagName("h2")[0].innerHTML="Hi";  //Collection(array)
+      document.getElementsByClassName("head2")[0].innerHTML="Hi Hi Hi";
---
+++ date-objects-and-methods.js
@@ -0,0 +1,9 @@
+
+var date=new Date();
+console.log(date);
+
+var month=date.getMonth();
+console.log(month);
+
+var day=date.getDay();
+console.log(day);
