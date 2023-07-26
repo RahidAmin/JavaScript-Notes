b339a145a69227350ed3aeb3f570989a132f5136
RahidAmin <rahidamin19@gmail.com>
2023-07-25 21:11:44 -0700

    Query Selector

--- Dom.js
+++ Dom.js
@@ -6,3 +6,16 @@
 
      document.getElementsByTagName("h2")[0].innerHTML="Hi";  //Collection(array)
       document.getElementsByClassName("head2")[0].innerHTML="Hi Hi Hi";
+
+      //-----------Query Selector----
+
+      var qrySelector=document.querySelector("a").innerHTML="This is Home";
+      document.querySelector("li a").innerHTML="This is Tutorial & Bangladesh";
+      document.querySelector(".myDiv a").innerHTML="Tag";
+      document.querySelectorAll("p")[0].innerHTML="Pragraph";
+      document.querySelectorAll("p")[1].innerHTML="Pragraph";
+
+
+     
+     
+      
